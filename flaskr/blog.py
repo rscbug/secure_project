0cd8cb33ad0b85b5486502a30daf24be6805cb8e
Alex Bo <rsc_bug@mail.ru>
2025-02-24 10:23:55 +0300

    Add encrypt and decrypt post

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -9,6 +9,7 @@ from werkzeug.exceptions import abort
 
 from .auth import login_required
 from .db import get_db
+from .utils import encrypt_data, get_hash, decrypt_data
 
 bp = Blueprint("blog", __name__)
 
@@ -24,19 +25,25 @@ def index():
     ).fetchall()
     return render_template("blog/index.html", posts=posts)
 
+@bp.route("/show")
+@login_required
+def show():
+    """Show all the posts and decrypt body for login user"""
+    db = get_db()
+    posts = db.execute(
+        "SELECT p.id, title, body, created, author_id, username"
+        " FROM post p JOIN user u ON p.author_id = u.id"
+        " ORDER BY created DESC"
+    ).fetchall()
+
+    for post in posts:
+        if g.user['id'] == post['author_id']:
+            post["body"]=decrypt_data(get_hash(g.user['username']), str(g.user['id'])*16, post["body"])
+    return render_template("blog/index.html", posts=posts)
+
 
 def get_post(id, check_author=True):
-    """Get a post and its author by id.
-
-    Checks that the id exists and optionally that the current user is
-    the author.
-
-    :param id: id of post to get
-    :param check_author: require the current user to be the author
-    :return: the post with author information
-    :raise 404: if a post with the given id doesn't exist
-    :raise 403: if the current user isn't the author
-    """
+    """Get a post and its author by id."""
     post = (
         get_db()
         .execute(
@@ -63,22 +70,22 @@ def create():
     """Create a new post for the current user."""
     if request.method == "POST":
         title = request.form["title"]
-        body = request.form["body"]
+        body = request.form.get("body", "Null")
         error = None
-
+               
         if not title:
             error = "Title is required."
 
         if error is not None:
             flash(error)
         else:
-            db = get_db()
+            db = get_db()            
             db.execute(
                 "INSERT INTO post (title, body, author_id) VALUES (?, ?, ?)",
-                (title, body, g.user["id"]),
+                (title, encrypt_data(get_hash(g.user['username']), str(g.user['id'])*16, body), g.user["id"]),
             )
             db.commit()
-            return redirect(url_for("blog.index"))
+            return redirect(url_for("blog.show"))
 
     return render_template("blog/create.html")
 
@@ -88,12 +95,13 @@ def create():
 def update(id):
     """Update a post if the current user is the author."""
     post = get_post(id)
-
+    post["body"]=decrypt_data(get_hash(g.user['username']), str(g.user['id'])*16, post["body"])
+       
     if request.method == "POST":
         title = request.form["title"]
         body = request.form["body"]
         error = None
-
+        
         if not title:
             error = "Title is required."
 
@@ -102,10 +110,10 @@ def update(id):
         else:
             db = get_db()
             db.execute(
-                "UPDATE post SET title = ?, body = ? WHERE id = ?", (title, body, id)
+                "UPDATE post SET title = ?, body = ? WHERE id = ?", (title, encrypt_data(get_hash(g.user['username']), str(g.user['id'])*16, body), id)
             )
             db.commit()
-            return redirect(url_for("blog.index"))
+            return redirect(url_for("blog.show"))
 
     return render_template("blog/update.html", post=post)
 
@@ -113,11 +121,7 @@ def update(id):
 @bp.route("/<int:id>/delete", methods=("POST",))
 @login_required
 def delete(id):
-    """Delete a post.
-
-    Ensures that the post exists and that the logged in user is the
-    author of the post.
-    """
+    """Delete a post."""
     get_post(id)
     db = get_db()
     db.execute("DELETE FROM post WHERE id = ?", (id,))
