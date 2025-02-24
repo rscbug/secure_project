f8acef5f880b2a5be1a5203a920b13811f5ec786
Alex Bo <rsc_bug@mail.ru>
2025-02-24 10:24:20 +0300

    Add dict_factory

--- flaskr/db.py
+++ flaskr/db.py
@@ -6,24 +6,25 @@ from flask import current_app
 from flask import g
 
 
+
+def dict_factory(cursor, row):
+        d = {}  
+        for idx, col in enumerate(cursor.description):
+            d[col[0]] = row[idx]
+        return d
+
+
 def get_db():
-    """Connect to the application's configured database. The connection
-    is unique for each request and will be reused if this is called
-    again.
-    """
     if "db" not in g:
         g.db = sqlite3.connect(
             current_app.config["DATABASE"], detect_types=sqlite3.PARSE_DECLTYPES
         )
-        g.db.row_factory = sqlite3.Row
+        g.db.row_factory = dict_factory
 
     return g.db
 
 
 def close_db(e=None):
-    """If this request connected to the database, close the
-    connection.
-    """
     db = g.pop("db", None)
 
     if db is not None:
@@ -31,7 +32,6 @@ def close_db(e=None):
 
 
 def init_db():
-    """Clear existing data and create new tables."""
     db = get_db()
 
     with current_app.open_resource("schema.sql") as f:
@@ -40,7 +40,6 @@ def init_db():
 
 @click.command("init-db")
 def init_db_command():
-    """Clear existing data and create new tables."""
     init_db()
     click.echo("Initialized the database.")
 
@@ -49,8 +48,5 @@ sqlite3.register_converter("timestamp", lambda v: datetime.fromisoformat(v.decod
 
 
 def init_app(app):
-    """Register database functions with the Flask app. This is called by
-    the application factory.
-    """
     app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
